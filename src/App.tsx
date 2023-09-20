935e5d684842f396e8f41e3e51e50a0e394f5922
oMiguelwnl <contato.Miguel.almeida7@gmail.com>
2023-09-20 10:03:22 -0300

    useFetch & createContext

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import Header from './Components/Header';
+import Sidenav from './Components/Sidenav';
+import { DataContextProvider } from './Context/DataContext';
+import Resumo from './Pages/Resumo';
+import './Style.css';
+
+function App() {
+  return (
+    <DataContextProvider>
+      <div>
+        <Sidenav />
+        <main>
+          <Header />
+          <Resumo />
+        </main>
+      </div>
+    </DataContextProvider>
+  );
+}
+export default App;
---
+++ src/Context/DataContext.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import useFetch from '../Hooks/UseFetch';
+
+type IDataContext = {
+  loading: boolean;
+  error: string | null;
+  data: IVenda[] | null;
+};
+
+type IVenda = {
+  id: string;
+  nome: string;
+  preco: number;
+  status: 'pago' | 'processando' | 'falha';
+  pagamento: 'boleto' | 'cartao' | 'pix';
+  parcelas: number | null;
+  data: string;
+};
+
+const DataContext = React.createContext<IDataContext | null>(null);
+
+export const useData = () => {
+  const context = React.useContext(DataContext);
+  if (!context) throw new Error('useData precisa estar em DataContextProvider');
+  return context;
+};
+
+export const DataContextProvider = ({ children }: React.PropsWithChildren) => {
+  const { data, loading, error } = useFetch<IVenda[]>(
+    'https://data.origamid.dev/vendas/',
+  );
+  return (
+    <DataContext.Provider value={{ data, loading, error }}>
+      {children}
+    </DataContext.Provider>
+  );
+};
