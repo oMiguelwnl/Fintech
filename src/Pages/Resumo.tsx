c8a03caa66fc138108ed818c6854d54b7932b358
oMiguelwnl <contato.Miguel.almeida7@gmail.com>
2023-09-22 11:52:46 -0300

    Grafico

--- src/Pages/Resumo.tsx
+++ src/Pages/Resumo.tsx
@@ -1,3 +1,4 @@
+import GraficoVendas from '../Components/GraficoVendas';
 import { useData } from '../Context/DataContext';
 
 const Resumo = () => {
@@ -34,7 +35,9 @@ const Resumo = () => {
           </span>
         </div>
       </div>
-      <div className="box mb">Gráficos</div>
+      <div className="box mb">
+        <GraficoVendas data={data} />
+      </div>
     </section>
   );
 };
